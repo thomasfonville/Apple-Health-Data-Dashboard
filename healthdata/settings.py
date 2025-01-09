758ca456e5fe5fcfae9716d939faa3438a5a853a
thomasfonville <thomas@thomasfonville.com>
2025-01-09 13:18:20 -0600

    Update settings.py

--- healthdata/settings.py
+++ healthdata/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-r!0juatguuh7(a_)s=cuc&&mqdvl9tjjo8&#x08sr74f1gk^=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS=.railway.app
 
 
 # Application definition
